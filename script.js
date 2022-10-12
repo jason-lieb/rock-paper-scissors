18481940caf3039bc828129d76641b5da2bc1bb4
Jason <Jason.lieb@outlook.com>
2022-10-12 19:12:09 -0400

    getComputerChoice and playRound functions now work

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let getComputerChoice = function() {
     let randNum = Math.floor(Math.random() * 3);
-    let choice;
     if (randNum === 0) {
         choice = 'rock';
     } else if (randNum === 1) {
@@ -14,17 +13,30 @@ let getComputerChoice = function() {
 let playRound = function(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === 'rock') {
         if (computerSelection.toLowerCase() === 'rock') {
-
+            output = 'You tied!';
         } else if (computerSelection.toLowerCase() === 'paper') {
-
+            output = 'You lose! Paper beats Rock';
         } else {
-
+            output = 'You win! Rock beats Scissors';
         }
     }   else if (playerSelection.toLowerCase() === 'paper') {
-
+        if (computerSelection.toLowerCase() === 'rock') {
+            output = 'You win! Paper beats Rock';
+        } else if (computerSelection.toLowerCase() === 'paper') {
+            output = 'You tied!';
+        } else {
+            output = 'You lose! Scissors beats Paper'
+        }
     }   else {
-
+        if (computerSelection.toLowerCase() === 'rock') {
+            output = 'You lose! Rock beats Scissors'
+        } else if (computerSelection.toLowerCase() === 'paper') {
+            output = 'You win! Scissors beats paper'
+        } else {
+            output = 'You tied!';
+        }
     }
+    return output;
 }
 
 let game = function() {
@@ -32,4 +44,4 @@ let game = function() {
         console.log(playRound());
     }
 }
-console.log(getComputerChoice())
+console.log(playRound('scissors', getComputerChoice()))
